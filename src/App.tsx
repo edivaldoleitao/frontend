ded997b19f22844b5ec9886cd03578013125e433
Lucas Ferruccio <lucaspaiva.ufrpe@gmail.com>
2025-07-09 10:54:19 -0300

    Ajuste nas rotas do  App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import RegisterPage from "./pages/register";
 import UpgradePage from "./pages/upgrade";
 import EditProfile from "./pages/editProfile";
 import PrivateRoute from "./routes/PrivateRoute";
-import ProductPage from "./pages/productDetail/ProductDetail";
 import HomePage from "./pages/home";
 import ListProductsPage from "./pages/listProducts";
 import RecoverPasswordPage from "./pages/forgotPassword/recoverPassword/RecoverPassword";
 import ChangePasswordPage from "./pages/forgotPassword/changePassword/ChangePassword";
-import DealsPage from "./pages/deals";
 
 function App() {
   return (
@@ -24,10 +22,6 @@ function App() {
           <Route path="/CreateUser" element={<RegisterPage />} />
           <Route path="/RecoverPassword" element={<RecoverPasswordPage />} />
           <Route path="/ChangePassword/:id" element={<ChangePasswordPage />} />
-          <Route path="/" element={<InitialPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/CreateUser" element={<RegisterPage />} />
           <Route
             path="/EditProfile"
             element={
@@ -37,9 +31,6 @@ function App() {
             }
           />
           <Route path="/upgrade" element={<UpgradePage />} />
-          <Route path="/ofertas" element={<DealsPage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/deals" element={<DealsPage />} />
           <Route path="/produtos" element={<ListProductsPage />} />
         </Routes>
       </BrowserRouter>
